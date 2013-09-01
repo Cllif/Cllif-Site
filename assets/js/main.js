c196b18a8a4b33d4d0b8028e598b1afc4a50c166
Julio <juliohisloti@gmail.com>
2013-09-01 19:54:44 -0300

    Corrigindo bug de dimensão

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,18 @@ $(function (){
 	$('#bg-home').maximage();
 
 	//Pegando Altura
+	$(window).load(altura);
+  	$(window).resize(altura);
 	function altura(){
-		
-	}
-	var height = document.documentElement.clientHeight;
-	//Colocar valor no elento
-	if(height > 650){
+		var height = document.documentElement.clientHeight;
+		//Colocar valor no elento
+		if(height > 650){
 
-		$('.page').show().css({'height':height + "px"});	
+			$('.page').show().css({'height':height + "px"});	
 
-	}else{
-		//Nada
+		}else{
+			//Nada
+		}	
 	}
 
 });	
